cde92932b8ea02c1eb784937de97cb1c6da7826b
Adam Howard <adam.howard.music@gmail.com>
2021-01-30 19:54:10 -0600

    Consolidate to one 'get'

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,7 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-router.get("/", function (req, res, next) {
-  res.sendFile("/index.html", {
-    root: path.join(__dirname, "../public"),
-  });
-});
-
-router.get("/play/", function (req, res, next) {
-  res.sendFile("/index.html", {
-    root: path.join(__dirname, "../public"),
-  });
-});
-
-router.get("/welcome/", function (req, res, next) {
-  res.sendFile("/index.html", {
-    root: path.join(__dirname, "../public"),
-  });
-});
-
-router.get("/waiting-room/", function (req, res, next) {
+router.get("/:type(welcome|play|waiting-room)/", function (req, res, next) {
   res.sendFile("/index.html", {
     root: path.join(__dirname, "../public"),
   });
